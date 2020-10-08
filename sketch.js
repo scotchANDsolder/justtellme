07a52986baa6fc54fe0548a625b13168e4c8856c
scotchandsolder <narulaab@msu.edu>
2020-10-08 19:24:33 -0400

    trying gyro

--- sketch.js
+++ sketch.js
@@ -1,16 +1,18 @@
-var x, y, z;
+var alpha, beta, gamma;
 var xpos, ypos;
 
 function setup()
 {
   // set canvas size
-  createCanvas(400, 400);
+  var myCanvas = createCanvas(400, 400);
+  myCanvas.parent('sketch');
 
   // default values
   xpos = 200;
   ypos = 200;
-  x = 0;
-  y = 0;
+  alpha = 0;
+  beta = 0;
+  gamma = 0;
 }
 
 function draw()
@@ -18,33 +20,28 @@ function draw()
   // set background color to white
   background(255);
 
-  // add/subract xpos and ypos
-  xpos = xpos + x;
-  ypos = ypos - y;
-
-  // wrap ellipse if over bounds
-  if(xpos > 400) { xpos = 0; }
-  if(xpos < 0) { xpos = 400; }
-  if(ypos > 400) { ypos = 0; }
-  if(ypos < 0) { ypos = 400; }
-
-  // draw ellipse
-  fill(255, 0, 0);
-  ellipse(xpos, ypos, 25, 25);
-
   // display variables
-  fill(0);
+  fill(100);
   noStroke();
-  text("x: " + x, 25, 25);
-  text("y: " + y, 25, 50);
-  text("z: " + z, 25, 75);
+  text("alpha: " + alpha, 25, 25);
+  text("beta: " + beta, 25, 50);
+  fill(255, 0, 0);
+  text("gamma: " + gamma, 25, 75);
+
+  push();
+  translate(200, 200);
+  rotate(radians(gamma));
+  fill(255, 0, 0);
+  rect(-40, -40, 80, 80);
+  fill(0);
+  rect(-40, 40, 80, 5);
+  pop();
 }
 
 // accelerometer Data
-window.addEventListener('devicemotion', function(e)
+window.addEventListener('deviceorientation', function(e)
 {
-  // get accelerometer values
-  x = parseInt(e.accelerationIncludingGravity.x);
-  y = parseInt(e.accelerationIncludingGravity.y);
-  z = parseInt(e.accelerationIncludingGravity.z);
+  alpha = e.alpha;
+  beta = e.beta;
+  gamma = e.gamma;
 });
