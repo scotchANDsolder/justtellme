7ffc48aa1ae4e1a134a02b8b306bb2b02a92ee69
narulaab <narulaab@msu.edu>
2020-10-08 17:01:36 -0400

    testing mobile accerlation sensor

--- sketch.js
+++ sketch.js
@@ -1,15 +1,50 @@
+var x, y, z;
+var xpos, ypos;
 
-function setup() {
-  // put setup code here
-createCanvas(700,700);
-background(200,10,120);
+function setup()
+{
+  // set canvas size
+  createCanvas(400, 400);
 
+  // default values
+  xpos = 200;
+  ypos = 200;
+  x = 0;
+  y = 0;
 }
 
-function draw() {
-  fill(30,40,200);
-  circle(20,20,70);
-  fill(200,40,200);
-  rect(400,400,100,50);
+function draw()
+{
+  // set background color to white
+  background(255);
 
+  // add/subract xpos and ypos
+  xpos = xpos + x;
+  ypos = ypos - y;
+
+  // wrap ellipse if over bounds
+  if(xpos > 400) { xpos = 0; }
+  if(xpos < 0) { xpos = 400; }
+  if(ypos > 400) { ypos = 0; }
+  if(ypos < 0) { ypos = 400; }
+
+  // draw ellipse
+  fill(255, 0, 0);
+  ellipse(xpos, ypos, 25, 25);
+
+  // display variables
+  fill(0);
+  noStroke();
+  text("x: " + x, 25, 25);
+  text("y: " + y, 25, 50);
+  text("z: " + z, 25, 75);
 }
+
+// accelerometer Data
+window.addEventListener('devicemotion', function(e)
+{
+  // get accelerometer values
+  x = parseInt(e.accelerationIncludingGravity.x);
+  y = parseInt(e.accelerationIncludingGravity.y);
+  z = parseInt(e.accelerationIncludingGravity.z);
+});
