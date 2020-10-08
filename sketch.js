4e0277c72438e3c292e1fc8ccd2fead09793a664
narulaab <narulaab@msu.edu>
2020-10-08 13:11:28 -0400

    added p5j lib

---
+++ sketch.js
@@ -0,0 +1,15 @@
+
+function setup() {
+  // put setup code here
+createCanvas(700,700);
+background(200,10,120);
+
+}
+
+function draw() {
+  fill(30,40,200);
+  circle(20,20,70);
+  fill(200,40,200);
+  rect(400,400,100,50);
+
+}
